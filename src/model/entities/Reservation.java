9ee62dd0a5e2fe20081588a7da73d973b94023b5
Roger <rogerbritosan@gmail.com>
2023-08-25 15:00:24 -0300

    good solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -30,39 +30,30 @@ public class Reservation {
 		return checkIn;
 	}
 
-//	public void setCheckIn(Date checkIn) {
-//		this.checkIn = checkIn;
-//	}
-
 	public Date getCheckOut() {
 		return checkOut;
 	}
-
-//	public void setCheckOut(Date checkOut) {
-//		this.checkOut = checkOut;
-//	}
 
 	public long duration() {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) {
 		
 		Date now = new Date();
 
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for update must be future dates. ";
+			throw new IllegalArgumentException("Reservation dates for update must be future dates. ");
 		}
 		
 		if (!checkOut.after(checkIn)) {
-			return "Check-out date must be after check-in date";
+			throw new IllegalArgumentException("Check-out date must be after check-in date");
 		}
 		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 		
-		return null;
 	}
 	
 	@Override
